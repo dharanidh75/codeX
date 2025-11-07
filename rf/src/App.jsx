65af036d5ff8208c05a66178b7409788a572278b
dharanidh75 <dharanidh777@gmail.com>
2025-11-07 15:11:21 +0530

    update

--- rf/src/App.jsx
+++ rf/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GetStarted from "./getstarted.jsx";
 import Profile from "./profile.jsx";
