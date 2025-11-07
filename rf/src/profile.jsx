6bc8596a9844b5ce5ae0fe5372f5c1006e5a71d7
dharanidh75 <dharanidh777@gmail.com>
2025-11-08 00:12:10 +0530

    update

--- rf/src/profile.jsx
+++ rf/src/profile.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import"./profile.css"
-import Navbar from "./components/navbar.jsx"
-import Footer from"./components/footer.jsx"
-import { useNavigate } from 'react-router-dom';
-function profile() {
+import React from 'react';
+import './profile.css';
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
+
+function Profile() {
   return (
-    <div>
+    <div className="page-container">
       <header>
         <Navbar />
       </header>
-      <div className='main-content'>
-        <div className='content-left'>
-          <img src="src/assets/profileLogo.png" alt="Profile Logo" className='profileLogo'/>
-        </div>
-        <div className='content-right'>
-          <h1 className='profile-head'>Profile</h1>
-            <p><strong>Name:</strong> Karthik</p>
-            <p><strong>Username:</strong> karthik@123</p>
-            <p><strong>Email:</strong>karthik12@gmail.com</p>
-            <p><strong>Team:</strong>TeamX</p>
-        </div>
-      </div>
+
+        <main className="mai-content">
+          <div className="content-left">
+            <img
+              src="src/assets/profileLogo.png"
+              alt="Profile Logo"
+              className="profileLogo"
+            />
+            <p className='✏️ changeIcon'>change icon</p>
+          </div>
+          <div className='line'></div>
+          <div className="content-right">
+            <h1 className="profile-head">Profile</h1>
+            <p className='p'><strong>Name:</strong> Karthik</p>
+            <p className='p'><strong>Gender:</strong> Male</p>
+            <p className='p'><strong>Username:</strong> karthik@123</p>
+            <p className='p'><strong>Email:</strong> karthik12@gmail.com</p>
+            <p className='p'><strong>Team:</strong> TeamX</p>
+          </div>
+      </main>
+
       <Footer />
     </div>
-  )
+  );
 }
 
-export default profile
+export default Profile;
